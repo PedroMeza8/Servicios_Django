306604ad3e2d99fbfb08e454bab3a888adf52731
PedroMeza8 <pedromeza8@hotmail.com>
2021-07-30 19:03:12 -0300

    Creacion de filtros de movimiento

--- ServiciosDjango/soporte/views.py
+++ ServiciosDjango/soporte/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Servicios, Movimientos
+from .models import Clientes, Servicios, Movimientos
 
 # Create your views here.
 
@@ -13,6 +13,14 @@ def servicios(request):
     return render(request,'servicios.html', context={'servicios':listaServicios}) 
 
 def movimientos(request):
+    if request.method == 'POST':
+        dni = request.POST.get('dni')
+        cliente1 = Clientes.objects.get(DNI = dni)
+        print(cliente1.DNI, 'Mi dni')
+        # resultado= cliente1.movimientos_set.all()
+        resultado = Movimientos.objects.filter(cliente__DNI = dni)
+        print(resultado)
+        return render(request,'movimientos.html', context={'movimientos':resultado})
     listaMovimientos= Movimientos.objects.all()
     print(listaMovimientos)
     
